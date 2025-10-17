e0377f9dbac3a5ebc6e8ac430c5226024893847d
Oussama <benatik@gmail.com>
2025-10-17 22:08:40 +0100

    ajout dependance PrimeFaces

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,6 +1,20 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
+/* Copier le contenu d'un textarea dans le clipboard */
+async function copyToClipboard(idTextArea) {
+    var textArea = document.getElementById("form:" + idTextArea);
+    if(textArea) {
+        try {
+            await navigator.clipboard.writeText(textArea.value);
+        } catch (err) {
+            console.error("Erreur lors de la copie : ", err)
+        }
+    }
+    // Cet ancien code est deprecated :
+    // textArea.select();
+    // document.execCommand('copy');
+}
 
-
+/* Effacer la dernière question et la dernière réponse */
+function toutEffacer() {
+    document.getElementById("form:question").value = "";
+    document.getElementById("form:reponse").value = "";
+}
